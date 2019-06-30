9a4eff0b078f733a0bca9de2949cdb43d0b80d16
Janne <Janne555@users.noreply.github.com>
2019-06-30 11:54:43 +0300

    add flush promises

--- setupTests.js
+++ setupTests.js
@@ -2,3 +2,11 @@ const Dexie = require('dexie')
 
 Dexie.dependencies.indexedDB = require('fake-indexeddb')
 Dexie.dependencies.IDBKeyRange = require('fake-indexeddb/lib/FDBKeyRange')
+
+//eslint-disable-next-line
+global.flushPromises = function flushPromises() {
+  return new Promise(resolve => {
+    //eslint-disable-next-line
+    setImmediate(resolve())
+  })
+}
