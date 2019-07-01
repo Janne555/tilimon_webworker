ea155a6420dbe44285b0a44cb02905defd5d7b1c
Janne <Janne555@users.noreply.github.com>
2019-07-01 07:18:38 +0300

    add more constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,15 @@
 export const EVENT_ROW = 'eventRow'
-export const TABLES = [EVENT_ROW]
-
 export const DATE = 'date'
 export const AMOUNT = 'amount'
 export const EVENT_TYPE = 'eventType'
 export const DESCRIPTION = 'description'
+export const FILTER = 'filter'
+export const TAG = 'tag'
+export const NAME = 'name'
+export const MODULE = 'module'
+export const FILTER_GROUP = 'filterGroup'
+
+export const TABLES = [EVENT_ROW, FILTER, TAG, MODULE, FILTER_GROUP]
 
 export const PUT = 'put'
 export const GET = 'get'
@@ -12,3 +17,7 @@ export const DELETE = 'delete'
 
 export const SINGLE = 'single'
 export const BULK = 'bulk'
+
+export const EQUALS = 'equals'
+export const ANY_OF = 'anyOf'
+export const NONE_OF = "noneOf"
