27d244ecd3a1b12cd4d110079b16ae11936993ef
Janne <Janne555@users.noreply.github.com>
2019-07-02 07:54:18 +0300

    implement getter

--- src/database/getHandler.js
+++ src/database/getHandler.js
@@ -1,18 +1,16 @@
-import { EVENT_ROW, FILTER, FILTER_GROUP, TAG, MODULE } from "../constants";
-import { getEventRow } from "./getEventRow";
+import { EVENT_ROW, FILTER, FILTER_GROUP, TAG, MODULE } from "../constants"
+import { getEventRow } from "./getEventRow"
+import db from './database'
 
-export function getHandler({ table, filterGroupId, ...rest }) {
+export function getHandler({ table, filterGroupId }) {
   switch (table) {
     case EVENT_ROW:
       return getEventRow(filterGroupId)
     case FILTER:
-      throw Error("not implemented yet")
     case FILTER_GROUP:
-      throw Error("not implemented yet")
     case TAG:
-      throw Error("not implemented yet")
     case MODULE:
-      throw Error("not implemented yet")
+      return db[table].toCollection().toArray()
     default:
       throw Error("tried to get from table that is undefined")
   }
