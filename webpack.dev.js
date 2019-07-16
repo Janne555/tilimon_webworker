e3575c017c6cbc172bf7ac1ae66d97e0f78a2039
Janne <Janne555@users.noreply.github.com>
2019-07-16 14:39:32 +0300

    fiz

--- webpack.dev.js
+++ webpack.dev.js
@@ -12,7 +12,13 @@ module.exports = merge(common, {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      {
+        test: /\.js$/, exclude: /node_modules/, loader: "babel-loader",
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ["@babel/plugin-proposal-class-properties"]
+        }
+      }
     ]
   }
 })
