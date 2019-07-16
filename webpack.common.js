d91ee3d401686d790c06a970431c5e57c3f11d95
Janne <Janne555@users.noreply.github.com>
2019-07-16 14:10:53 +0300

    fiiiix

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,6 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
